31c0b0abf12a4b6d0044a9319c1be10e6196059b
Eremin Dmitry <mail@eremindmitry.ru>
2017-11-27 10:30:58 +0300

    Started to add mouse module

--- sources/libpassion/include/modules/ps_mouse.h
+++ sources/libpassion/include/modules/ps_mouse.h
@@ -17,4 +17,69 @@ PS_PRIVATE_API enum ps_status ps_mouse_deinitialize(
         struct ps_passion *this
 );
 
+PS_PUBLIC_API enum ps_status ps_mouse_get_position(
+        struct ps_passion *this,
+        uint16_t *x, uint16_t *y
+);
+
+PS_PUBLIC_API enum ps_status ps_mouse_get_relative_mode(
+        struct ps_passion *this,
+        bool *relative_mode
+);
+
+PS_PUBLIC_API enum ps_status ps_mouse_get_x(
+        struct ps_passion *this,
+        uint16_t *x
+);
+
+PS_PUBLIC_API enum ps_status ps_mouse_get_y(
+        struct ps_passion *this,
+        uint16_t *y
+);
+
+PS_PUBLIC_API enum ps_status ps_mouse_is_down(
+        struct ps_passion *this,
+        enum ps_mouse_button button
+);
+
+PS_PUBLIC_API enum ps_status ps_mouse_is_grabbed(
+        struct ps_passion *this,
+        bool *grabbed
+);
+
+PS_PUBLIC_API enum ps_status ps_mouse_is_visible(
+        struct ps_passion *this,
+        bool *visible
+);
+
+PS_PUBLIC_API enum ps_status ps_mouse_set_grabbed(
+        struct ps_passion *this,
+        bool grabbed
+);
+
+PS_PUBLIC_API enum ps_status ps_mouse_set_position(
+        struct ps_passion *this,
+        uint16_t x, uint16_t y
+);
+
+PS_PUBLIC_API enum ps_status ps_mouse_set_relative_mode(
+        struct ps_passion *this,
+        bool relative_mode
+);
+
+PS_PUBLIC_API enum ps_status ps_mouse_set_visible(
+        struct ps_passion *this,
+        bool visible
+);
+
+PS_PUBLIC_API enum ps_status ps_mouse_set_x(
+        struct ps_passion *this,
+        uint16_t x
+);
+
+PS_PUBLIC_API enum ps_status ps_mouse_set_y(
+        struct ps_passion *this,
+        uint16_t y
+);
+
 #endif
--- sources/libpassion/source/modules/ps_mouse.c
+++ sources/libpassion/source/modules/ps_mouse.c
@@ -1,5 +1,7 @@
 #include <passion.h>
 
+#include <SDL_mouse.h>
+
 enum ps_status ps_mouse_initialize(struct ps_passion *this)
 {
         PS_CHECK(this, PS_STATUS_INVALID_ARGUMENT);
@@ -14,6 +16,128 @@ enum ps_status ps_mouse_deinitialize(struct ps_passion *this)
         if (!this) {
                 return PS_STATUS_SUCCESS;
         }
+
+        return PS_STATUS_SUCCESS;
+}
+
+enum ps_status ps_mouse_get_position(struct ps_passion *this,
+        uint16_t *x, uint16_t *y
+)
+{
+        PS_CHECK(this && (x || y), PS_STATUS_INVALID_ARGUMENT);
+
+        return PS_STATUS_SUCCESS;
+}
+
+enum ps_status ps_mouse_get_relative_mode(struct ps_passion *this,
+        bool *relative_mode
+)
+{
+        PS_CHECK(this && relative_mode, PS_STATUS_INVALID_ARGUMENT);
+
+        return PS_STATUS_SUCCESS;
+}
+
+enum ps_status ps_mouse_get_x(struct ps_passion *this,
+        uint16_t *x
+)
+{
+        PS_CHECK(this && x, PS_STATUS_INVALID_ARGUMENT);
+
+        return PS_STATUS_SUCCESS;
+}
+
+enum ps_status ps_mouse_get_y(struct ps_passion *this,
+        uint16_t *y
+)
+{
+        PS_CHECK(this && y, PS_STATUS_INVALID_ARGUMENT);
+
+        return PS_STATUS_SUCCESS;
+}
+
+enum ps_status ps_mouse_is_down(struct ps_passion *this,
+        enum ps_mouse_button button
+)
+{
+        PS_CHECK(this, PS_STATUS_INVALID_ARGUMENT);
+
+        return PS_STATUS_SUCCESS;
+}
+
+enum ps_status ps_mouse_is_grabbed(struct ps_passion *this,
+        bool *grabbed
+)
+{
+        PS_CHECK(this && grabbed, PS_STATUS_INVALID_ARGUMENT);
+
+        return PS_STATUS_SUCCESS;
+}
+
+enum ps_status ps_mouse_is_visible(struct ps_passion *this,
+        bool *visible
+)
+{
+        PS_CHECK(this && visible, PS_STATUS_INVALID_ARGUMENT);
+
+        return PS_STATUS_SUCCESS;
+}
+
+enum ps_status ps_mouse_set_grabbed(struct ps_passion *this,
+        bool grabbed
+)
+{
+        PS_CHECK(this, PS_STATUS_INVALID_ARGUMENT);
+
+        return PS_STATUS_SUCCESS;
+}
+
+enum ps_status ps_mouse_set_position(struct ps_passion *this,
+        uint16_t x, uint16_t y
+)
+{
+        PS_CHECK(this, PS_STATUS_INVALID_ARGUMENT);
+
+        return PS_STATUS_SUCCESS;
+}
+
+enum ps_status ps_mouse_set_relative_mode(struct ps_passion *this,
+        bool relative_mode
+)
+{
+        PS_CHECK(this, PS_STATUS_INVALID_ARGUMENT);
+
+        return PS_STATUS_SUCCESS;
+}
+
+enum ps_status ps_mouse_set_visible(struct ps_passion *this,
+        bool visible
+)
+{
+        PS_CHECK(this, PS_STATUS_INVALID_ARGUMENT);
+
+        return PS_STATUS_SUCCESS;
+}
+
+enum ps_status ps_mouse_set_x(struct ps_passion *this,
+        uint16_t x
+)
+{
+        PS_CHECK(this, PS_STATUS_INVALID_ARGUMENT);
+
+        uint16_t current_x = 0;
+        uint16_t current_y = 0;
+
+        PS_STATUS_ASSERT(ps_mouse_get_position(this, &current_x, &current_y));
+
+        return PS_STATUS_SUCCESS;
+}
+
+enum ps_status ps_mouse_set_y(struct ps_passion *this,
+        uint16_t y
+)
+{
+        PS_CHECK(this, PS_STATUS_INVALID_ARGUMENT);
 
         return PS_STATUS_SUCCESS;
 }
