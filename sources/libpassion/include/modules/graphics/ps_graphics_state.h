b32845201f66070328ee3410ab580167ac9ffed2
Eremin Dmitry <mail@eremindmitry.ru>
2017-12-07 11:34:49 +0300

    Added ps_graphics_(get/set)_line_join

--- sources/libpassion/include/modules/graphics/ps_graphics_state.h
+++ sources/libpassion/include/modules/graphics/ps_graphics_state.h
@@ -3,6 +3,8 @@
 #include "../utils/ps_api.h"
 #include "../utils/ps_status.h"
 
+#include "graphics/ps_line_join.h"
+
 PS_PUBLIC_API enum ps_status ps_graphics_get_background_color(
         struct ps_passion *this,
         struct ps_color *color
@@ -11,6 +13,11 @@ PS_PUBLIC_API enum ps_status ps_graphics_get_background_color(
 PS_PUBLIC_API enum ps_status ps_graphics_get_color(
         struct ps_passion *this,
         struct ps_color *color
+);
+
+PS_PUBLIC_API enum ps_status ps_graphics_get_line_join(
+        struct ps_passion *this,
+        enum ps_line_join *line_join
 );
 
 PS_PUBLIC_API enum ps_status ps_graphics_is_active(
@@ -26,6 +33,11 @@ PS_PUBLIC_API enum ps_status ps_graphics_set_background_color(
 PS_PUBLIC_API enum ps_status ps_graphics_set_color(
         struct ps_passion *this,
         double red, double green, double blue, double alpha
+);
+
+PS_PUBLIC_API enum ps_status ps_graphics_set_line_join(
+        struct ps_passion *this,
+        enum ps_line_join join
 );
 
 #endif
--- sources/libpassion/source/modules/graphics/ps_graphics_state.c
+++ sources/libpassion/source/modules/graphics/ps_graphics_state.c
@@ -39,6 +39,24 @@ enum ps_status ps_graphics_get_color(struct ps_passion *this,
         return PS_STATUS_SUCCESS;
 }
 
+enum ps_status ps_graphics_get_line_join(struct ps_passion *this,
+        enum ps_line_join *line_join
+)
+{
+        PS_CHECK(this && line_join, PS_STATUS_INVALID_ARGUMENT);
+
+        struct ps_display_state *state = NULL;
+        PS_STATUS_ASSERT(
+                ps_display_state_list_get_last(
+                        &this->graphics.display_states, &state
+                )
+        );
+
+        *line_join = state->line_join;
+
+        return PS_STATUS_SUCCESS;
+}
+
 enum ps_status ps_graphics_is_active(struct ps_passion *this, bool *active)
 {
         PS_CHECK(this && active, PS_STATUS_INVALID_ARGUMENT);
@@ -84,6 +102,24 @@ enum ps_status ps_graphics_set_color(struct ps_passion *this,
         state->foreground_color.green = green;
         state->foreground_color.blue = blue;
         state->foreground_color.alpha = alpha;
+
+        return PS_STATUS_SUCCESS;
+}
+
+enum ps_status ps_graphics_set_line_join(struct ps_passion *this,
+        enum ps_line_join line_join
+)
+{
+        PS_CHECK(this, PS_STATUS_INVALID_ARGUMENT);
+
+        struct ps_display_state *state = NULL;
+        PS_STATUS_ASSERT(
+                ps_display_state_list_get_last(
+                        &this->graphics.display_states, &state
+                )
+        );
+
+        state->line_join = line_join;
 
         return PS_STATUS_SUCCESS;
 }
